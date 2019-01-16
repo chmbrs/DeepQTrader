6642fae9638c508aaabecf4066128b675bca212b
Chmbrs <juanjosechambers@gmail.com>
2019-01-16 13:10:42 -0200

    Python 3 enabled

--- agent/agent.py
+++ agent/agent.py
@@ -10,6 +10,7 @@ from collections import deque
 
 class Agent:
 	def __init__(self, state_size, is_eval=False, model_name=""):
+
 		self.state_size = state_size # normalized previous days
 		self.action_size = 3 # sit, buy, sell
 		self.memory = deque(maxlen=1000)
@@ -25,26 +26,31 @@ class Agent:
 		self.model = load_model("models/" + model_name) if is_eval else self._model()
 
 	def _model(self):
+
 		model = Sequential()
+
 		model.add(Dense(units=64, input_dim=self.state_size, activation="relu"))
 		model.add(Dense(units=32, activation="relu"))
 		model.add(Dense(units=8, activation="relu"))
 		model.add(Dense(self.action_size, activation="linear"))
+
 		model.compile(loss="mse", optimizer=Adam(lr=0.001))
 
 		return model
 
 	def act(self, state):
-		if not self.is_eval and np.random.rand() <= self.epsilon:
+		if not self.is_eval and random.random() <= self.epsilon:
 			return random.randrange(self.action_size)
 
 		options = self.model.predict(state)
 		return np.argmax(options[0])
 
 	def expReplay(self, batch_size):
+
 		mini_batch = []
 		l = len(self.memory)
-		for i in xrange(l - batch_size + 1, l):
+
+		for i in range(l - batch_size + 1, l):
 			mini_batch.append(self.memory[i])
 
 		for state, action, reward, next_state, done in mini_batch:
--- functions.py
+++ functions.py
@@ -1,9 +1,11 @@
 import numpy as np
 import math
 
+
 # prints formatted price
 def formatPrice(n):
 	return ("-$" if n < 0 else "$") + "{0:.2f}".format(abs(n))
+
 
 # returns the vector containing stock data from a fixed file
 def getStockDataVec(key):
@@ -15,16 +17,30 @@ def getStockDataVec(key):
 
 	return vec
 
+
 # returns the sigmoid
 def sigmoid(x):
-	return 1 / (1 + math.exp(-x))
+	try:
+		ans = 1 / (1 + math.exp(-x))
+	except OverflowError:
+		print('erro')
+		ans = 1 / (1 + float('inf'))
+
+	return ans
+
 
 # returns an an n-day state representation ending at time t
 def getState(data, t, n):
+	# d = 0 - window_size + 1
 	d = t - n + 1
+
 	block = data[d:t + 1] if d >= 0 else -d * [data[0]] + data[0:t + 1] # pad with t0
+	print(f'block {block}')
+
 	res = []
-	for i in xrange(n - 1):
+	for i in range(n - 1):
 		res.append(sigmoid(block[i + 1] - block[i]))
+
+	print(f'sigmoid {res}')
 
 	return np.array([res])
